f6f2f0d366b2b59669035f5739433844fb40a423
chinaq <aaa6263@sina.com>
2021-05-25 11:01:25 +0800

    showed books on web

--- angular/src/app/book/book.component.ts
+++ angular/src/app/book/book.component.ts
@@ -1,15 +1,25 @@
+import { ListService, PagedResultDto } from '@abp/ng.core';
 import { Component, OnInit } from '@angular/core';
+// import { BookService, BookDto } from '@proxy/books';
+import { BookService } from '@proxy/application/books';
+import { BookDto } from '@proxy/application/contracts/books';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  styleUrls: ['./book.component.scss'],
+  providers: [ListService],
 })
 export class BookComponent implements OnInit {
+  book = { items: [], totalCount: 0 } as PagedResultDto<BookDto>;
 
-  constructor() { }
+  constructor(public readonly list: ListService, private bookService: BookService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    const bookStreamCreator = (query) => this.bookService.getList(query);
+
+    this.list.hookToQuery(bookStreamCreator).subscribe((response) => {
+      this.book = response;
+    });
   }
-
 }
