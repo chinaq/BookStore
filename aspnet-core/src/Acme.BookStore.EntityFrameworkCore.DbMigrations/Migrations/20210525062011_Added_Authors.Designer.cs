ad4b10fcb2a1d2d5eddbeb401e282f6a1631a9e9
chinaq <aaa6263@sina.com>
2021-05-25 14:35:54 +0800

    Authors Database Integration

--- aspnet-core/src/Acme.BookStore.EntityFrameworkCore.DbMigrations/Migrations/20210525062011_Added_Authors.Designer.cs
+++ aspnet-core/src/Acme.BookStore.EntityFrameworkCore.DbMigrations/Migrations/20210525062011_Added_Authors.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Acme.BookStore.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
 namespace Acme.BookStore.Migrations
 {
     [DbContext(typeof(BookStoreMigrationsDbContext))]
-    partial class BookStoreMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210525062011_Added_Authors")]
+    partial class Added_Authors
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
